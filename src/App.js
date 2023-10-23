07e003176f817978da805886e7267453c2bda5bc
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2023-10-23 21:31:34 +0400

    sidebar finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Sidebar from "./components/Sidebar.jsx";
 import AllTask from "./components/AllTasks.jsx";
+import { useState } from "react";
 
 function App() {
+  const [category, setCategory] = useState('All');
+
   return (
       <div className='Container'>
-        <Sidebar />
+        <Sidebar onCategoryClicked={chosenCategory => setCategory(chosenCategory)} chosenCategory={category} />
         <AllTask />
       </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,21 @@
-const Sidebar = () => {
-    return ( 
-        <div className="categories">
-            <ul>
-                <li>All</li>
-                <li>Groceries</li>
-                <li>College</li>
-                <li>Payments</li>
-            </ul>
-        </div>
-     );
-}
- 
+const Sidebar = ({ chosenCategory, onCategoryClicked }) => {
+  const allCategories = ["All", "Groceries", "College", "Payment"];
+
+  return (
+    <div className="categories">
+      <ul>
+        {allCategories.map((category) => {
+          return (
+            <li
+              onClick={(e) => onCategoryClicked(category)}
+              style={{
+                color: chosenCategory === category ? "#EA5959" : "#525252",
+              }}>{category}</li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
 export default Sidebar;
