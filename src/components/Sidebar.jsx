8ccc44b41a36e0525d218d8f7ea1c3e5ab66b092
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2023-10-23 20:35:42 +0400

    changes

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+    return ( 
+        <div className="categories">
+            <ul>
+                <li>All</li>
+                <li>Groceries</li>
+                <li>College</li>
+                <li>Payments</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default Sidebar;
