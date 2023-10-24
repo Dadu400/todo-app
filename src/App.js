52259246c185d145f2b63a35ddba95f9201ec155
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2023-10-24 11:39:09 +0400

    finalChanges

--- src/App.js
+++ src/App.js
@@ -3,13 +3,45 @@ import AllTask from "./components/AllTasks.jsx";
 import { useState } from "react";
 
 function App() {
-  const [category, setCategory] = useState('All');
+  const [category, setCategory] = useState("All");
+  const [todos, setTodos] = useState([]);
+
+  const onNewTodoAdded = (todo) => {
+    const newTodos = [...todos]
+    newTodos.unshift(todo)
+    setTodos(newTodos);
+  };
+
+  const onTodoUpdated = (updatedTodo) => {
+    const updatedTodos = []
+    for (const todo of todos) {
+      if (todo.title === updatedTodo.title) {
+        todo.checked = updatedTodo.checked;
+      }
+      updatedTodos.push(todo);
+    }
+
+    setTodos(updatedTodos);
+  }
+
+  const onTodoDeleted = (todoToDeleted) => {
+    setTodos(todos.filter((todo) => todo !== todoToDeleted))
+  }
 
   return (
-      <div className='Container'>
-        <Sidebar onCategoryClicked={chosenCategory => setCategory(chosenCategory)} chosenCategory={category} />
-        <AllTask />
-      </div>
+    <div className="Container">
+      <Sidebar
+        onCategoryClicked={(chosenCategory) => setCategory(chosenCategory)}
+        chosenCategory={category}
+      />
+      <AllTask
+        category={category}
+        onNewTodoAdded={onNewTodoAdded}
+        todos={todos.filter((todo) => category === 'All' || todo.category === category)}
+        onTodoUpdated={onTodoUpdated}
+        onTodoDeleted={onTodoDeleted}
+      />
+    </div>
   );
 }
 
--- src/components/AllTasks.jsx
+++ src/components/AllTasks.jsx
@@ -1,24 +1,82 @@
-import bin from './bin.svg';
+import { useState } from "react";
+import bin from "./bin.svg";
 
-const AllTask = () => {
-    return (  
-        <div className="tasks">
-            <h1>All Tasks</h1>
-            <input type="text" placeholder="Add a new task inside 'All' category" />
-            <div className="todo">
-                <div className='todosname'>
-                    <label className='custom-checkbox'>
-                    <input type="checkbox" id='checkbox'/>
-                    <span className='checkmark'></span>
-                    </label>
-                <p>Buy Milk</p>
-                </div>
-                <div>
-                <img src={bin} alt="bin" id='bin'/>
-                </div>
+const AllTask = ({ category, todos, onNewTodoAdded, onTodoUpdated, onTodoDeleted }) => {
+  const [hoveredTodo, setHoveredTodo] = useState(null);
+
+  const onNewTaskEntered = (e) => {
+    if (e.key === "Enter" && e.target.value !== "") {
+      const todo = {
+        title: e.target.value,
+        category: category,
+        checked: false,
+      };
+      e.target.value = "";
+      onNewTodoAdded(todo);
+    }
+  };
+
+  const onCheckboxClicked = (todo) => {
+    todo.checked = !todo.checked;
+    onTodoUpdated(todo);
+  };
+
+  const onTrashClicked = (todo) => {
+    onTodoDeleted(todo);
+  };
+
+  return (
+    <div className="tasks">
+      <h1>{category} Tasks</h1>
+      <input
+        type="text"
+        placeholder={`Add a new task inside '${category}' category`}
+        onKeyDown={onNewTaskEntered}/>
+      {todos.map((todo) => {
+        return (
+          <div className="todo" onMouseEnter={(e) => setHoveredTodo(todo)} onMouseLeave={(e) => setHoveredTodo(null)}>
+            <div className="todosname">
+              <label className="custom-checkbox">
+                <input
+                  type="checkbox"
+                  checked={todo.checked === true}
+                  onChange={(e) => onCheckboxClicked(todo)}
+                />
+                <span className="checkmark" 
+                style={{
+                    backgroundColor:
+                    todo.checked === true ? "#EA5959" : "#fff",
+                }}>
+                </span>
+              </label>
+              <p style={{ whiteSpace: "nowrap" }}>
+                <span style={{
+                    textDecoration:
+                      todo.checked === true ? "line-through" : "none",
+                  }}>
+                  {todo.title}
+                </span>
+                <span className="todoCategory">
+                  {todo.category === "All" ? "Uncategorized" : todo.category}
+                </span>
+              </p>
             </div>
-        </div>
-    );
-}
- 
+            <div>
+              <img
+                src={bin}
+                alt="bin"
+                className="trash"
+                style={{ 
+                    display: hoveredTodo === todo ? "block": "none"
+                 }}
+                onClick={(e) => onTrashClicked(todo)}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
 export default AllTask;
